b5d7ed45e738cf875cb511044a5d5672b39a923f
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-01 09:16:12 +0900

    [js]send message

--- app/assets/javascripts/messag.js
+++ app/assets/javascripts/messag.js
@@ -1,4 +1,23 @@
 $(function () {
+  function buildHTML(message) {
+    var image = message.image ? `<img src= ${message.image}>` : "";
+    var html = `<div class="message">
+                    <div class="message__upper-info">
+                      <p class="message__upper-info__talker">
+                      ${message.user_name}
+                      </p >
+                      <p class="message__upper-info__date">
+                        ${message.created_at.strftime("%Y/%m/%d %H:%M")}
+                      </p>
+                    </div >
+                      <p class="message__text"></p>
+                      <p class="lower-message__content">
+                        ${message.content}
+                      <%= image_tag ' ${image}' %>
+                    </p>
+                  </div > `
+    return html;
+  }
   $('#new_message').on('submit', function (e) {
     e.preventDefault();
     console.log(this)
@@ -12,5 +31,13 @@ $(function () {
       processData: false,
       contentType: false
     })
+      .done(function (data) {
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.form__message').val('');
+      })
+      .fail(function (content) {
+        alert('送信が失敗しました。');
+      })
   })
 })
