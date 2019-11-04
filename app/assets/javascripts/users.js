4de3b65dcd58d2caad8fdf3a728e0454e198702b
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-04 19:41:23 +0900

    const html,add_html

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,8 +2,16 @@ $(function () {
 
   function html(name) {
     const html = `<div class="chat-group-user clearfix">
-    <p class="chat-group-user__name">${name}</p></div>`;
+                  <p class="chat-group-user__name">${name}</p>
+                  </div>`;
     $("#user-search-result").append(html)
+  }
+  function add_html(name, id) {
+    const add_html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
+      <input name='group[user_ids][]' type='hidden' value='${id}'>
+      <p class='chat-group-user__name'>${name}</p>
+      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>追加</a></div>`;
+    $(".js-add-user").append(add_html)
   }
 
   $("#user-search-field").on("keyup", function () {
