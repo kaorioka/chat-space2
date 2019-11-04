4fe9641eb16f1c0951764df4c8ecc3f21e012a46
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-04 18:43:50 +0900

    add ajax

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,18 @@
 $(function () {
   $("#user-search-field").on("keyup", function () {
     let input = $("#user-search-field").val();
-    console.log(input);
+    console.log(input)
+    $.ajax({
+      type: "GET",
+      url: "/users/search",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function () {
+        console.log("成功です");
+      })
+      .fail(function () {
+        console.log("失敗です");
+      });
   });
 });
