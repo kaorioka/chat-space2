02ef8e9fc61aac2f63920437ad0d8be62c0ecbda
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-04 19:31:30 +0900

    add function html(name)

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,11 @@
 $(function () {
+
+  function html(name) {
+    const html = `<div class="chat-group-user clearfix">
+    <p class="chat-group-user__name">${name}</p></div>`;
+    $("#user-search-result").append(html)
+  }
+
   $("#user-search-field").on("keyup", function () {
     let input = $("#user-search-field").val();
     console.log(input)
@@ -11,8 +18,8 @@ $(function () {
       .done(function () {
         console.log("成功です");
       })
-      .fail(function () {
-        console.log("失敗です");
-      });
-  });
+  })
+    .fail(function () {
+      console.log("失敗です");
+    });
 });
