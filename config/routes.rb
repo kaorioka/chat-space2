249eb15038e9a0853da359b4319671cda4a5342b
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-04 22:36:26 +0900

    edti routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "groups#index"
-  resources :users, only: [:edit,:update] do
-    get 'search'
-  end
+  resources :users, only: [:edit,:update]
+  get 'users/search' => 'users#search'
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
