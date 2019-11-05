d611cc3942cb3a38db3e28667d96b74781734389
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-05 23:59:00 +0900

    addGroupUsers action

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,22 +1,34 @@
 $(function () {
 
+  $(document).on('turbolinks:load', function () {
 
-  function outputResult(user) {
-    var html =
-      `<div class="chat-group-user clearfix">
+    // search_result users template
+    function outputResult(user) {
+      var html =
+        `<div class="chat-group-user clearfix">
         <p class="chat-group-user__name">
           ${user.name}</p>
         <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
         </a>
       </div>
     `;
-  }
+    } // search_result users template //
 
 
-  $(document).on('turbolinks:load', function () {
+    // search_result users template
+    function addGroupUsers(id, name) {
+      var html =
+        `<div class="chat-group-user clearfix" id=chat-group-user-${id}>
+        <input type="hidden" name="group[user_ids][]" value="${id}">
+        <p class="chat-group-user__name">${name}</p>
+        <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}">削除</a>
+      </div>`;
+    } // search_result users template//
+
     const seach_field = $('#user-search-field')
     const search_result = $('#user-search-result')
 
+    // Incremental search
     seach_field.on('keyup', function () {
 
       let input = seach_field.val();
@@ -34,8 +46,16 @@ $(function () {
         .fail(function (data) {
           alert("検索に失敗しました");
         });
-    }); //seach_field.on >>
+    }); // Incremental search //
 
+    // addGroupUsers action
+    search_result.on('click', '.chat-group-user__btn--add', function (e) {
+      e.preventDefault();
+      var id = $(this).data('userId');
+      var name = $(this).data('userName');
+      addGroupUsers(id, name); //add action
+      $(this).parent('.chat-group-user').remove(); //delete action
+    });  //search_result.on('click')>>
 
 
 
