c0c274731cf8cdaa22837f486427ec286ce5a097
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-05 23:22:06 +0900

    const seach_field,search_result

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,23 +1,32 @@
 $(function () {
 
   $(document).on('turbolinks:load', function () {
+    const seach_field = $('#user-search-field')
+    const search_result = $('#user-search-result')
 
-    $('#user-search-field').on('keyup', function () {
+    seach_field.on('keyup', function () {
+
+      let input = seach_field.val();
+      console.log(input);
+
       $.ajax({
         type: 'GET',
-        url: '/users/search',
+        url: '/users/search', //request URL
         date: { keyword: input },
-        dateType: 'json'
+        dataType: 'json'
       })
-        .dane(function () {
+        .done(function () {
 
         })
-        .fail(function () {
-
+        .fail(function (data) {
+          alert("検索に失敗しました");
         });
+    }); //seach_field.on >>
 
-    });
+
+
+
+
 
   });
-
 });
