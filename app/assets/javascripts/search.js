7670a51bab239feecd8e88e8af94829de5612a5a
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-05 22:16:56 +0900

    add_ajax

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,18 @@
 $(function () {
 
+  $(document).on('turbolinks:load', function () {
 
+    $('#user-search-field').on('keyup', function () {
+      $.ajax({
+        type: 'GET',
+        url: '/users/search',
+        date: { keyword: input },
+        dateType: 'json'
+      });
+
+
+    });
+
+  });
 
 });
