621943760048be6c14bce204b095eefd78436936
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-05 20:47:15 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "groups#index"
-  resources :users, only: [:edit,:update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+  resources :users, only: [:edit,:update] do
     collection do
       get 'search'
     end
+  end
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   end
 
 end
