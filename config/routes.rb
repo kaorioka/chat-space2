0ca9495e2e04059454dda48c9810709eb3ee02d5
Kaori Oka <kaorioka09jm@gmail.com>
2019-11-06 14:10:32 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
     resources :messages, only: [:index, :create]
   end
 
+  namespace :api do
+    resources :messages, only: :index, defaults: { format: 'json' }
+  end
+
 end
